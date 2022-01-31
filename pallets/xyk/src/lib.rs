bd92a243082c9c3b80c19da98dfc8a15bc840c93
i1i1 <vanyarybin1@live.ru>
2022-01-31 18:32:07 +0300

    Create xyk pallet
    Signed-off-by: i1i1 <vanyarybin1@live.ru>

--- pallets/xyk/src/lib.rs
+++ pallets/xyk/src/lib.rs
@@ -1,8 +1,10 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
-/// Edit this file to define custom logic or remove it if it is not needed.
-/// Learn more about FRAME and the core library of Substrate FRAME pallets:
-/// <https://docs.substrate.io/v3/runtime/frame>
+use codec::{Decode, Encode};
+use frame_support::traits::Get;
+use frame_system::RawOrigin;
+use sp_runtime::traits::{Hash, StaticLookup};
+
 pub use pallet::*;
 
 #[cfg(test)]
@@ -11,92 +13,293 @@ mod mock;
 #[cfg(test)]
 mod tests;
 
-#[cfg(feature = "runtime-benchmarks")]
-mod benchmarking;
+impl<T: Config> Pallet<T> {
+    fn transfer(
+        from: T::AccountId,
+        to: T::AccountId,
+        asset_id: AssetId<T>,
+        amount: Balance<T>,
+    ) -> Result<(), Error<T>> {
+        pallet_assets::Pallet::<T>::transfer(
+            RawOrigin::Signed(from).into(),
+            asset_id.into(),
+            T::Lookup::unlookup(to),
+            amount,
+        )
+        .map_err(|_| Error::<T>::NotEnoughTokens)
+    }
+
+    fn mint_lp(lp: AssetId<T>, amount: Balance<T>, to: T::AccountId) {
+        pallet_assets::Pallet::<T>::mint(
+            RawOrigin::Signed(T::DEXAddr::get()).into(),
+            lp.into(),
+            T::Lookup::unlookup(to),
+            amount,
+        )
+        .unwrap()
+    }
+
+    fn burn_lp(lp: AssetId<T>, amount: Balance<T>, from: T::AccountId) {
+        pallet_assets::Pallet::<T>::burn(
+            RawOrigin::Signed(T::DEXAddr::get()).into(),
+            lp.into(),
+            T::Lookup::unlookup(from),
+            amount,
+        )
+        .unwrap()
+    }
+
+    pub fn lp_token(x: AssetId<T>, y: AssetId<T>) -> AssetId<T> {
+        let (x, y) = if x > y { (y, x) } else { (x, y) };
+        AssetId::<T>::decode(&mut T::AssetIdHash::hash_of(&(x, y)).encode().as_ref()).unwrap()
+    }
+}
 
 #[frame_support::pallet]
 pub mod pallet {
-    use frame_support::pallet_prelude::*;
-    use frame_system::pallet_prelude::*;
+    use codec::{Decode, Encode};
+    use frame_support::{
+        dispatch::Parameter,
+        ensure,
+        pallet_prelude::*,
+        storage::{with_transaction, TransactionOutcome},
+    };
+    use frame_system::{pallet_prelude::*, RawOrigin};
+    use scale_info::TypeInfo;
+    use sp_runtime::traits::{Hash, StaticLookup};
+
+    use core::cmp::Ord;
 
     /// Configure the pallet by specifying the parameters and types on which it depends.
     #[pallet::config]
-    pub trait Config: frame_system::Config {
-        /// Because this pallet emits events, it depends on the runtime's definition of an event.
+    pub trait Config: frame_system::Config + pallet_assets::Config {
         type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
+
+        type AssetId: IsType<<Self as pallet_assets::Config>::AssetId> + Parameter + Ord + Copy;
+        /// Address where dex stores all the funds
+        type DEXAddr: Get<Self::AccountId>;
+        /// Minimum balance for issued LP tokens
+        type LpMinBalance: Get<Self::Balance>;
+        /// Hash for asset ids. It is used to determenistically get LP asset id from 2 asset ids.
+        type AssetIdHash: Hash;
     }
+
+    pub type AssetId<T> = <T as Config>::AssetId;
+    pub type Balance<T> = <T as pallet_assets::Config>::Balance;
 
     #[pallet::pallet]
     #[pallet::generate_store(pub(super) trait Store)]
     pub struct Pallet<T>(_);
 
-    // The pallet's runtime storage items.
-    // https://docs.substrate.io/v3/runtime/storage
-    #[pallet::storage]
-    #[pallet::getter(fn something)]
-    // Learn more about declaring storage items:
-    // https://docs.substrate.io/v3/runtime/storage#declaring-storage-items
-    pub type Something<T> = StorageValue<_, u32>;
+    #[derive(Debug, Clone, Copy, Encode, Decode, TypeInfo)]
+    #[scale_info(skip_type_params(T))]
+    pub struct Pair<T: Config> {
+        x_id: AssetId<T>,
+        x_balance: Balance<T>,
+        y_id: AssetId<T>,
+        y_balance: Balance<T>,
+    }
 
-    // Pallets use events to inform users when important changes are made.
-    // https://docs.substrate.io/v3/runtime/events-and-errors
+    #[pallet::storage]
+    pub type LPPairs<T> = StorageMap<_, Blake2_128Concat, AssetId<T>, Pair<T>>;
+
     #[pallet::event]
     #[pallet::generate_deposit(pub(super) fn deposit_event)]
     pub enum Event<T: Config> {
-        /// Event documentation should end with an array that provides descriptive names for event
-        /// parameters. [something, who]
-        SomethingStored(u32, T::AccountId),
+        Swapped {
+            who: T::AccountId,
+            from: (AssetId<T>, Balance<T>),
+            to: (AssetId<T>, Balance<T>),
+        },
+        Locked {
+            who: T::AccountId,
+            x: (AssetId<T>, Balance<T>),
+            y: (AssetId<T>, Balance<T>),
+            lp: (AssetId<T>, Balance<T>),
+        },
+        Unlocked {
+            who: T::AccountId,
+            x: (AssetId<T>, Balance<T>),
+            y: (AssetId<T>, Balance<T>),
+            lp: (AssetId<T>, Balance<T>),
+        },
+        LPMinted {
+            who: T::AccountId,
+            x: (AssetId<T>, Balance<T>),
+            y: (AssetId<T>, Balance<T>),
+            lp: (AssetId<T>, Balance<T>),
+        },
     }
 
-    // Errors inform users that something went wrong.
+    /// Errors inform users that something went wrong.
     #[pallet::error]
     pub enum Error<T> {
-        /// Error names should be descriptive.
-        NoneValue,
-        /// Errors should have helpful documentation associated with them.
-        StorageOverflow,
+        /// No liquidity available
+        NoLiquidity,
+        /// Not enough tokens
+        NotEnoughTokens,
+        LiquidityAlreadyExists,
     }
 
-    // Dispatchable functions allows users to interact with the pallet and invoke state changes.
-    // These functions materialize as "extrinsics", which are often compared to transactions.
-    // Dispatchable functions must be annotated with a weight and must return a DispatchResult.
     #[pallet::call]
     impl<T: Config> Pallet<T> {
-        /// An example dispatchable that takes a singles value as a parameter, writes the value to
-        /// storage and emits an event. This function must be dispatched by a signed extrinsic.
-        #[pallet::weight(10_000 + T::DbWeight::get().writes(1))]
-        pub fn do_something(origin: OriginFor<T>, something: u32) -> DispatchResult {
-            // Check that the extrinsic was signed and get the signer.
-            // This function will return an error if the extrinsic is not signed.
-            // https://docs.substrate.io/v3/runtime/origins
+        /// Swap 2 tokens
+        #[pallet::weight(10_000)]
+        pub fn swap(
+            origin: OriginFor<T>,
+            from_id: AssetId<T>,
+            from_balance: Balance<T>,
+            to_id: AssetId<T>,
+        ) -> DispatchResult {
             let who = ensure_signed(origin)?;
+            let lp_id = Self::lp_token(from_id, to_id);
 
-            // Update storage.
-            <Something<T>>::put(something);
+            let to_amount = LPPairs::<T>::mutate(lp_id, |pair| {
+                let pair = pair.as_mut().ok_or(Error::<T>::NoLiquidity)?;
 
-            // Emit an event.
-            Self::deposit_event(Event::SomethingStored(something, who));
-            // Return a successful DispatchResultWithPostInfo
+                Self::transfer(who.clone(), T::DEXAddr::get(), from_id, from_balance)?;
+
+                // x * y = k
+                // (x + dx) * (y - dy) = k
+                // y - dy = x * y / (x + dx)
+                // dy = y - x * y / (x + dx)
+                let new_x = pair.x_balance + from_balance;
+                let new_y = (pair.x_balance * pair.y_balance) / new_x;
+
+                let dy = pair.y_balance - new_y;
+
+                Self::transfer(T::DEXAddr::get(), who.clone(), to_id, dy)
+                    .expect("Must always work by because of x*y=k architecture");
+
+                pair.x_balance = new_x;
+                pair.y_balance = new_y;
+
+                Ok::<_, Error<T>>(dy)
+            })?;
+
+            let to = (to_id, to_amount);
+            let from = (from_id, from_balance);
+            Self::deposit_event(Event::Swapped { who, from, to });
             Ok(())
         }
 
-        /// An example dispatchable that may throw a custom error.
-        #[pallet::weight(10_000 + T::DbWeight::get().reads_writes(1,1))]
-        pub fn cause_error(origin: OriginFor<T>) -> DispatchResult {
-            let _who = ensure_signed(origin)?;
+        /// Create new lp pair
+        #[pallet::weight(10_000)]
+        pub fn create_lp(
+            origin: OriginFor<T>,
+            x_id: AssetId<T>,
+            x_balance: Balance<T>,
+            y_id: AssetId<T>,
+            y_balance: Balance<T>,
+        ) -> DispatchResult {
+            let who = ensure_signed(origin)?;
 
-            // Read a value from storage.
-            match <Something<T>>::get() {
-                // Return an error if the value has not been set.
-                None => Err(Error::<T>::NoneValue)?,
-                Some(old) => {
-                    // Increment the value read from storage; will error in the event of overflow.
-                    let new = old.checked_add(1).ok_or(Error::<T>::StorageOverflow)?;
-                    // Update the value in storage with the incremented result.
-                    <Something<T>>::put(new);
+            let lp_id = Self::lp_token(x_id, y_id);
+
+            ensure!(!LPPairs::<T>::contains_key(lp_id), Error::<T>::LiquidityAlreadyExists);
+
+            let do_transfer = || -> DispatchResult {
+                Self::transfer(who.clone(), T::DEXAddr::get(), x_id, x_balance)?;
+                Self::transfer(who.clone(), T::DEXAddr::get(), y_id, y_balance)?;
+                Ok(())
+            };
+
+            with_transaction(|| match do_transfer() {
+                r @ Ok(()) => TransactionOutcome::Commit(r),
+                e @ Err(_) => TransactionOutcome::Rollback(e),
+            })?;
+
+            pallet_assets::Pallet::<T>::create(
+                RawOrigin::Signed(T::DEXAddr::get()).into(),
+                lp_id.into(),
+                T::Lookup::unlookup(T::DEXAddr::get()),
+                T::LpMinBalance::get(),
+            )
+            .expect("Must always be random. Assume no collisions");
+
+            LPPairs::<T>::insert(lp_id, Pair { x_id, x_balance, y_id, y_balance });
+            Self::mint_lp(lp_id, x_balance * y_balance, who.clone());
+
+            let x = (x_id, x_balance);
+            let y = (y_id, y_balance);
+            let lp = (lp_id, x_balance * y_balance);
+            Self::deposit_event(Event::LPMinted { who, x, y, lp });
+            Ok(())
+        }
+
+        /// Lock liquidity
+        #[pallet::weight(10_000)]
+        pub fn lock(
+            origin: OriginFor<T>,
+            x_id: AssetId<T>,
+            x_balance: Balance<T>,
+            y_id: AssetId<T>,
+        ) -> DispatchResult {
+            let who = ensure_signed(origin)?;
+
+            let lp_id = Self::lp_token(x_id, y_id);
+
+            let (y_amount, lp) = LPPairs::<T>::mutate(lp_id, |pair| {
+                let pair = pair.as_mut().ok_or(Error::<T>::NoLiquidity)?;
+
+                // dy / y = dx / x
+                // dy = y * dx / x
+                let dy = x_balance * pair.y_balance / pair.x_balance;
+
+                let lp_supply = pallet_assets::Pallet::<T>::total_supply(lp_id.into());
+                let lp = lp_supply * x_balance / pair.x_balance;
+
+                let do_transfer = || -> Result<(), Error<T>> {
+                    Self::transfer(who.clone(), T::DEXAddr::get(), x_id, x_balance)?;
+                    Self::transfer(who.clone(), T::DEXAddr::get(), y_id, dy)?;
                     Ok(())
-                }
-            }
+                };
+
+                with_transaction(|| match do_transfer() {
+                    r @ Ok(()) => TransactionOutcome::Commit(r),
+                    e @ Err(_) => TransactionOutcome::Rollback(e),
+                })?;
+
+                pair.x_balance += x_balance;
+                pair.y_balance += dy;
+
+                Self::mint_lp(lp_id, lp, who.clone());
+
+                Ok::<_, Error<_>>((dy, (lp_id, lp)))
+            })?;
+
+            let x = (x_id, x_balance);
+            let y = (y_id, y_amount);
+            Self::deposit_event(Event::Locked { who, x, y, lp });
+            Ok(())
+        }
+
+        /// Unlock liquidity
+        #[pallet::weight(10_000)]
+        pub fn unlock(origin: OriginFor<T>, lp: (AssetId<T>, Balance<T>)) -> DispatchResult {
+            let who = ensure_signed(origin)?;
+            let (lp_id, lp_balance) = lp;
+
+            let (x, y) = LPPairs::<T>::mutate(lp_id, |pair| {
+                let pair = pair.as_mut().ok_or(Error::<T>::NoLiquidity)?;
+
+                let lp_supply = pallet_assets::Pallet::<T>::total_supply(lp_id.into());
+
+                let dx = pair.x_balance * lp_balance / lp_supply;
+                let dy = pair.y_balance * lp_balance / lp_supply;
+
+                Self::burn_lp(lp_id, lp_balance, who.clone());
+
+                Self::transfer(T::DEXAddr::get(), who.clone(), pair.x_id, dx)
+                    .expect("Dex should always have enough tokens");
+                Self::transfer(T::DEXAddr::get(), who.clone(), pair.y_id, dy)
+                    .expect("Dex should always have enough tokens");
+
+                Ok::<_, Error<T>>(((pair.x_id, dx), (pair.y_id, dy)))
+            })?;
+
+            Self::deposit_event(Event::Unlocked { who, x, y, lp });
+            Ok(())
         }
     }
 }
--- pallets/xyk/src/tests.rs
+++ pallets/xyk/src/tests.rs
@@ -1,13 +1,28 @@
-use crate::{mock::*, Error};
+use crate::{mock::*, pallet::AssetId, Error};
 use frame_support::{assert_noop, assert_ok};
 
+type Assets = pallet_assets::Pallet<Test>;
+type Xyk = crate::Pallet<Test>;
+
+const ADDR: AccountId = 0x1;
+
+fn create_pair_of_tokens() -> (AssetId<Test>, AssetId<Test>) {
+    dbg!(Assets::create(Origin::signed(1), 0x1337, ADDR, 1)).unwrap();
+    dbg!(Assets::create(Origin::signed(1), 0x1338, ADDR, 1)).unwrap();
+    dbg!(Assets::mint(Origin::signed(1), 0x1337, ADDR, 1_000_000)).unwrap();
+    dbg!(Assets::mint(Origin::signed(1), 0x1338, ADDR, 1_000_000)).unwrap();
+    (0x1337, 0x1338)
+}
+
 #[test]
-fn it_works_for_default_value() {
+fn create_lp() {
     new_test_ext().execute_with(|| {
+        let (x, y) = create_pair_of_tokens();
+        // Xyk::create_lp(Origin::signed(1), (x, 1000), (y, 100_000)).unwrap();
         // Dispatch a signed extrinsic.
-        assert_ok!(TemplateModule::do_something(Origin::signed(1), 42));
+        // assert_ok!(Xyk::do_something(Origin::signed(1), 42));
         // Read pallet storage and assert an expected result.
-        assert_eq!(TemplateModule::something(), Some(42));
+        // assert_eq!(TemplateModule::something(), Some(42));
     });
 }
 
@@ -15,6 +30,6 @@ fn it_works_for_default_value() {
 fn correct_error_for_none_value() {
     new_test_ext().execute_with(|| {
         // Ensure the expected error is thrown when no value is present.
-        assert_noop!(TemplateModule::cause_error(Origin::signed(1)), Error::<Test>::NoneValue);
+        // assert_noop!(TemplateModule::cause_error(Origin::signed(1)), Error::<Test>::NoneValue);
     });
 }
